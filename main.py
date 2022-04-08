28cfcb29217c1097556a64b84413bc9ea7ae8884
Mayya <mayya.mukhina@icloud.com>
2022-04-08 04:04:03 +0300

    main

--- main.py
+++ main.py
@@ -2,19 +2,19 @@ import psycopg2
 from flask import Flask
 
 
-DATABASE_URL = os.environ['DATABASE_URL']
-con = psycopg2.connect(DATABASE_URL)
-cur = con.cursor()
+#DATABASE_URL = os.environ['DATABASE_URL']
+#con = psycopg2.connect(DATABASE_URL)
+#cur = con.cursor()
 
-query = f"""SELECT name FROM users """
+#query = f"""SELECT name FROM users """
 
-con.close()
+#con.close()
 
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return query[0]
+    return "query[0]"
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
