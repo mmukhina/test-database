1eafaaf8f31b5347f02f3e9c8b21683ba8ee0976
Mayya <mayya.mukhina@icloud.com>
2022-04-12 20:38:08 +0300

    test

--- main.py
+++ main.py
@@ -2,19 +2,32 @@ import psycopg2
 from flask import Flask
 
 
-#DATABASE_URL = os.environ['DATABASE_URL']
-#con = psycopg2.connect(DATABASE_URL)
-#cur = con.cursor()
-
-#query = f"""SELECT name FROM users """
-
-#con.close()
-
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-    return "query[0]"
+    host = "ec2-3-211-6-217.compute-1.amazonaws.com"
+    name = "d9ig9ca9682pc6"
+    user = "ijyzomfmfgkqla"
+    password = "d1d0923a4e5d9727ad012bbe8f412289b305b0415d129577b3310c3929af7933"
+
+    conn = psycopg2.connect(dbname = name, user = user, password = password, host = host)
+
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM users;")
+    result = str((cur.fetchall())[0][0])
+
+    cur.close()
+
+    conn.close()
+    print(result)
+    
+    return result
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
+
+
+
+
