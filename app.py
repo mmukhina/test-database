f4ee5aa464142a5a21dee297d06d5ec405386539
Mayya <mayya.mukhina@icloud.com>
2022-04-28 23:38:28 +0300

    new

--- app.py
+++ app.py
@@ -5,13 +5,18 @@ import os
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route('/<name>')
+def index(name):
+    return render_template('index.html', name=name)
 
 @socketio.on("message")
 def handleMessage(data):
     emit("new_message",data,broadcast=True)
+
+@socketio.on('connect')
+def test_connect():
+    emit("connected",broadcast=True)
     
 if __name__ == "__main__":
+    #socketio.run(app, port=8080, host='127.0.0.1')
     socketio.run(app)
