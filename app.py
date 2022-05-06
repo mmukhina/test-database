f9702bd2969a7a4657607cbb5325c5e389dc55af
Mayya <mayya.mukhina@icloud.com>
2022-05-06 19:46:14 +0300

    new

--- app.py
+++ app.py
@@ -1,22 +1,8 @@
-from flask import Flask, render_template, make_response, redirect
-from flask_socketio import SocketIO, send, emit
-import os
+from flask import Flask
+
+UPLOAD_FOLDER = 'static/uploads/'
 
 app = Flask(__name__)
-socketio = SocketIO(app)
-
-@app.route('/<name>')
-def index(name):
-    return render_template('index.html', name=name)
-
-@socketio.on("message")
-def handleMessage(data):
-    emit("new_message",data,broadcast=True)
-
-@socketio.on('connect')
-def test_connect():
-    emit("connected",broadcast=True)
-    
-if __name__ == "__main__":
-    #socketio.run(app, port=8080, host='127.0.0.1')
-    socketio.run(app)
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
